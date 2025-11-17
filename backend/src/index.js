efa69002b4a9ee41ef141eb176449d4be3b53bf2
saadicollection18 <saadicollection18@gmail.com>
2025-11-17 12:00:03 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,3 @@
 import app  from "./app.js";
-import connectDb from "./db/db.js";
-import dotenv from "dotenv";
-import { ApiError } from "./utils/apiError.js";
-
-dotenv.config({
-    path: ".env",
-});
-
-      
-
-const PORT=process.env.PORT
-connectDb().then(() => {
 
 
-        console.log("Server running on port ",PORT);
-  
-}).catch((error) => {
-
-    console.log("DB connection failed: ", error);
-    return new ApiError(500, "DB connection failed")
-});
